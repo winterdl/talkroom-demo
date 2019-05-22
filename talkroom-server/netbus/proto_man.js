2c512df6ffc02cef995d97b4904641b3029e3b1d
jianan <1072772483@qq.com>
2019-05-23 00:20:32 +0800

    只考虑json协议，代码量大幅度缩短

---
+++ talkroom-server/netbus/proto_man.js
@@ -0,0 +1,40 @@
+function encode_cmd(utag, stype, ctype, body) {
+    var buf = {};
+    buf[0] = stype;
+    buf[1] = ctype;
+    buf[2] = body;
+    buf[3] = utag;
+
+    var cmd_data = JSON.stringify(buf);
+
+    return cmd_data;
+}
+
+function decode_cmd(cmd_buf) { // 不再需要根据stype ctype定位解码器了，所以去掉了这2个参数
+    var cmd_data = null;
+
+    try{
+        cmd_data = JSON.parse(cmd_buf);
+    }catch (e) {
+        cmd_data = null;
+    }
+
+    return cmd_data; // 0  1  2  3 共4个字段
+}
+
+module.exports = {
+    encode_cmd: encode_cmd,
+    decode_cmd: decode_cmd
+};
+
+
+
+
+
+
+
+
+
+
+
+
